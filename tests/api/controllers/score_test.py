96a27b3ecb009afd42dd35f6c8b4c86f76bb6341
ElPapi42 <whitman-2@hotmail.com>
2020-05-11 13:46:40 -0400

    Add reverse tests for score endpoint

--- tests/api/controllers/score_test.py
+++ tests/api/controllers/score_test.py
@@ -31,3 +31,54 @@ def test_score(client, image, monkeypatch):
     )
 
     assert response.status_code == 200
+
+def test_bad_score(client, image, monkeypatch):
+    # Patch the external requests
+    patch_inference_request(image, monkeypatch)
+
+    # Create new entry in database
+    file = {'image': image}
+    response = client.post(
+        'api/inference/',
+        content_type='multipart/form-data',
+        data=file,
+    ).json
+    
+    # Assign score to the recently created example
+    data = {
+        'resource_id': response.get('resource_id'),
+        'score': 8,
+    }
+    response = client.put(
+        'api/score/',
+        json=data,
+    ).json
+
+    assert response == {
+        'message': 'Invalid request body arguments',
+        'payload': "{'score': ['Must be greater than or equal to 0 and less than or equal to 5.']}",
+    }
+
+def test_resource_not_found(client, image, monkeypatch):
+    # Patch the external requests
+    patch_inference_request(image, monkeypatch)
+
+    # Create new entry in database
+    file = {'image': image}
+    response = client.post(
+        'api/inference/',
+        content_type='multipart/form-data',
+        data=file,
+    ).json
+    
+    # Assign score to the recently created example
+    data = {
+        'resource_id': 'f45d6ced-29d8-49e1-a4ba-404792548cba',
+        'score': 3,
+    }
+    response = client.put(
+        'api/score/',
+        json=data,
+    ).json
+
+    assert response == {'message': 'Not found'}
